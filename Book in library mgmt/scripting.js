77bbed65d8520f1725cbd1afc342e196b410cf3a
dragonilly <134251470+JikokuAice@users.noreply.github.com>
2023-08-31 18:10:19 +0545

    library app complete with validations
    hmm after local storage and validation new library app is more advance and helpful

--- Book in library mgmt/scripting.js
+++ Book in library mgmt/scripting.js
@@ -1,51 +1,48 @@
 //form valiation 
+let dropit = document.getElementById('droping');
 let submit = document.querySelector('#main-button');
 const table = document.querySelector('.Realtable').getElementsByTagName('tbody')[0];
-submit.addEventListener('click',()=>{
-   
-    while(library.length > 0){
-        library.length=0;
-      console.log(library)
-        };
-        inputval();
-    fig();
-    
-    
-   
-    
+
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+let lib = JSON.parse(localStorage.getItem('book'))||[]
+for(const book of lib){
+    rowabove(book);
 }
-);
+})
 
-var library =[];
+
 function inputval(){
-   
-    var bookname = document.querySelector('.book-name').value;
-    var bookauthor = document.querySelector('.book-author').value;
-    var bookpage= document.querySelector('.book-page').value;
-    let dropit = document.getElementById('droping');
-
-
-
+    let bookname = document.querySelector('.book-name').value;
+    let bookauthor = document.querySelector('.book-author').value;
+    let bookpage= document.querySelector('.book-page').val
     var book ={
         bookNam : bookname,
         bookAuth : bookauthor,
         bookPage : bookpage
         };
-        console.log(book);
-       
-        let{bookNam,bookAuth,bookPage}=book; 
-
-
-library.push(bookNam,bookAuth,bookPage);
-
+   let lib = JSON.parse(localStorage.getItem('book'))||[];
+lib.push(book);
+let stringy = JSON.stringify(lib);
+localStorage.setItem('book',stringy);
 
 
 
 
+rowabove(book); 
+ }
 
-function rowabove(){
+
+
+
+
+function rowabove(book){
 
     let row = table.insertRow();
+row.style.borderBottom='1px solid rgba(0, 0, 0, 0.234)';
+row.style.fontWeight='bold';
+
     let cel1 = row.insertCell(0);
     let cel2 = row.insertCell(1);
     let cel3 = row.insertCell(2);
@@ -61,9 +58,9 @@ function rowabove(){
    delbtn.innerHTML='<i class="fa fa-times" aria-hidden="true"></i>';
    cel5.appendChild(delbtn);
         
-cel1.innerText=library[0]
-cel2.innerText=library[1];
-cel3.innerText=library[2];
+cel1.innerText=book.bookNam;
+cel2.innerText=book.bookAuth;
+cel3.innerText=book.bookPage;
 
 cel4.addEventListener('click', function(){
     btnintable.classList.toggle('black');
@@ -72,8 +69,8 @@ cel4.addEventListener('click', function(){
     }
     else{
        btnintable.innerText='Done'
-       btnintable.backgroundColor='white';
-       btnintable.color='black';
+       btnintable.style.backgroundColor='white';
+       btnintable.style.color='black';
     }
     });
     if(dropit.options[dropit.selectedIndex].value=='a2'){
@@ -83,60 +80,87 @@ cel4.addEventListener('click', function(){
 
     }
 
-delbtn.addEventListener('click',function(){
-    cel1.remove();
-    cel2.remove();
-    cel3.remove();
-    cel4.remove();
-   cel5.remove();
+   delbtn.addEventListener('click',()=>{
+    row.remove();
 
-   Array.length=0;
+    let lib = JSON.parse(localStorage.getItem('book'))||[]
+for(const index in lib){
 
-    });
+if(lib[index].bookPage==book.bookPage&&lib[index].bookAuth==book.bookAuth){
+lib.splice(index,1)
+}
+
+}
+
+localStorage.setItem('book',JSON.stringify(lib));
+
+});
+
+
+}
+
+let valid=document.querySelectorAll('#valid');
+function valiation(){
+    let bookname = document.querySelector('.book-name').value;
+    let bookauthor = document.querySelector('.book-author').value;
+    let bookpage= document.querySelector('.book-page').value;
+    let conditions;
+    if((bookauthor==''||bookpage==''||bookname=='')){
+valid.forEach((e)=>{
+e.style.display='block'
+e.innerText='error!no value'
+conditions=false;
+});
+
+}else{
+    valid.forEach((e)=>{
+        e.style.display=''
+        e.innerContent=''})
+        conditions=true;
+
+}
+
+return conditions;
+
+
+}
+
+
+function valiation2(){
    
-
-
-}
-rowabove();        
-}
-
-
-
-
-
-
-
-
-
-let bookname = document.querySelector('.book-name');
-let bookauthor = document.querySelector('.book-author');
-let bookpage= document.querySelector('.book-page');
-
-
-function fig(){
-
-
-    if((bookname.value)=='' ||  /^[0-9]+$/.test(bookname.value)){
-    alert(`error (1)! you didnt enter value
-
-     error(2) ${bookname.value} is number`);
-
-     bookname.value=''
+    let bookpage= document.querySelector('.book-page').value;
+    let valid2 = document.querySelectorAll('.validSec');
+    let conditions2;
     
-    }
+    if(isNaN(bookpage)){
+valid2.forEach((e)=>{
+e.style.display='block'
+e.innerText='letter only';
+conditions2=false;
+});
 
-    if(bookauthor.value=='' ||  /^[0-9]+$/.test(bookauthor.value)){
-        alert(`error (1)! you didnt enter value
+}else{
+    valid2.forEach((e)=>{
+        e.style.display=''
+        e.innerContent=''})
+        conditions2=true;
 
-        error(2) ${bookauthor.value}  is number`);
-
-        bookauthor.value=''
-    }
-
-    if(bookpage.value==''){
-        alert('please enter no of  pages');
-    }
 }
+
+return conditions2;
+
+
+}
+
+submit.addEventListener('click',()=>{
+if(valiation()){
+    if(valiation2()){
+inputval();
+    }
+}});
+    
+
+
 
 
 
